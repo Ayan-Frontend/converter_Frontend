62bca7432838d459255b77e1eeca8338bcd013fa
Ayan-Frontend <ayanalam3670@gmail.com>
2026-06-18 00:12:01 +0530

    fixing the API call

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   // baseURL: "http://localhost:5000/api",
-  baseURL: "https://converter-backend-dhhr.onrender.com",
+  baseURL: "https://converter-backend-dhhr.onrender.com/api",
   // https://converter-backend-dhhr.onrender.com
 });
 
